430c57190dccc95a017328456a3756a33c61bbb0
xcyuzhen <yuzhenxc@163.com>
2018-10-19 14:32:58 +0800

    1.更改serverconfog.json; 2.完成server配置拉取；

--- web-server/app.js
+++ web-server/app.js
@@ -22,6 +22,11 @@ app.configure('production', function(){
 	app.use(express.errorHandler());
 });
 
+app.get('/serverconfig', function (req, res) {
+	var serverConfig = require('./public/config/serverconfig.json');
+	res.send(JSON.stringify(serverConfig));
+});
+
 console.log("Web server has started.\nPlease log on http://127.0.0.1:3001/index.html");
 app.listen(3001);
 
