a271ca01bce44de16f13e22659301bc79d2f7f86
xcyuzhen <yuzhenxc@163.com>
2019-04-10 17:14:48 +0800

    更新服务端

--- game-server/app/consts/consts.js
+++ game-server/app/consts/consts.js
@@ -1,15 +1,17 @@
-module.exports = {
-	ROOM_SERVICE: {
-		ROOM_NUM: 100,
-	},
+var Consts = {};
 
-	ROOM: {
-		STATE: {
-			UN_INITED: 0, 						//房间未初始化
-			INITED: 1, 							//初始化完成
-			WAIT_TO_START: 2,					//人全部到齐，等待开局
-			PLAYING: 3, 						//开局中
-			ROUND_END: 4, 						//第一局结束，第二局还没开始
-		},
+Consts.ROOM_SERVICE = {
+	ROOM_NUM: 100,
+},
+
+Consts.ROOM = {
+	STATE: {
+		UN_INITED: 0, 						//房间未初始化
+		INITED: 1, 							//初始化完成
+		WAIT_TO_START: 2,					//人全部到齐，等待开局
+		PLAYING: 3, 						//开局中
+		ROUND_END: 4, 						//第一局结束，第二局还没开始
 	},
-};
+},
+
+module.exports = Consts;
--- game-server/app/consts/mjConsts.js
+++ game-server/app/consts/mjConsts.js
@@ -1,107 +1,108 @@
-module.exports = {
-	MSG_GROUP_NAME: "MjGroup",
+var Consts = {};
+Consts.MSG_GROUP_NAME = "MjGroup";
 
-	CARD_LIST: [
-		//万
-		0,1,2,3,4,5,6,7,8,
-		0,1,2,3,4,5,6,7,8,
-		0,1,2,3,4,5,6,7,8,
-		0,1,2,3,4,5,6,7,8,
-		//筒
-		9,10,11,12,13,14,15,16,17,
-		9,10,11,12,13,14,15,16,17,
-		9,10,11,12,13,14,15,16,17,
-		9,10,11,12,13,14,15,16,17,
-		//条
-		18,19,20,21,22,23,24,25,26,
-		18,19,20,21,22,23,24,25,26,
-		18,19,20,21,22,23,24,25,26,
-		18,19,20,21,22,23,24,25,26,
-	],
+Consts.CARD_LIST = [
+	//万
+	0,1,2,3,4,5,6,7,8,
+	0,1,2,3,4,5,6,7,8,
+	0,1,2,3,4,5,6,7,8,
+	0,1,2,3,4,5,6,7,8,
+	//筒
+	9,10,11,12,13,14,15,16,17,
+	9,10,11,12,13,14,15,16,17,
+	9,10,11,12,13,14,15,16,17,
+	9,10,11,12,13,14,15,16,17,
+	//条
+	18,19,20,21,22,23,24,25,26,
+	18,19,20,21,22,23,24,25,26,
+	18,19,20,21,22,23,24,25,26,
+	18,19,20,21,22,23,24,25,26,
+];
 
-	//马张数
-	MA_NUM: 6,
+//马张数
+Consts.MA_NUM = 6;
 
-	//游戏状态
-	GAME_STATE: {
-		INIT: 0, 																//初始化
-		FA_PAI: 1, 																//发牌
-		DA_PAI: 2, 																//打牌
-		RESULT: 3, 																//结算
-		OVER: 4, 																//结束
+//游戏状态
+Consts.GAME_STATE = {
+	INIT: 0, 																//初始化
+	FA_PAI: 1, 																//发牌
+	DA_PAI: 2, 																//打牌
+	RESULT: 3, 																//结算
+	OVER: 4, 																//结束
+};
+
+//操作类型
+Consts.OPE_TYPE = {
+	NO_OPT: -1, 															//无操作
+	GUO: 0, 																//过
+	PENG: 1, 																//碰
+	GANG: 2,																//杠
+	AN_GANG: 3,																//暗杠
+	BU_GANG: 4,																//补杠
+	HU: 5,																	//胡
+	OUT_CARD: 6, 															//打牌
+};
+
+//倍数类型
+Consts.RATE_TYPE = {
+	GANG: 1, 																//被人放杠
+	ZI_GANG: 2, 															//自摸杠
+	FANG_GANG: 3, 															//给别人放杠
+	BEI_ZI_GANG: 4, 														//被自摸杠
+	HU: 5, 																	//胡牌
+	BEI_HU: 6, 																//被胡牌
+	MO_MA: 7, 																//摸马
+	BEI_MA: 8, 																//被摸马
+};
+
+//倍数配置
+Consts.RATE_CONF = {
+	[Consts.RATE_TYPE.GANG]: 1, 											//倍数*(玩家数-1)
+	[Consts.RATE_TYPE.ZI_GANG]: 1, 											//倍数*(玩家数-1)
+	[Consts.RATE_TYPE.FANG_GANG]: -1, 										//倍数*(玩家数-1)
+	[Consts.RATE_TYPE.BEI_ZI_GANG]: -1, 									//倍数
+	[Consts.RATE_TYPE.HU]: 1, 												//倍数*(玩家数-1)
+	[Consts.RATE_TYPE.BEI_HU]: -1, 											//倍数
+	[Consts.RATE_TYPE.MO_MA]: 1, 											//倍数*(玩家数-1)*马数
+	[Consts.RATE_TYPE.BEI_MA]: -1, 											//倍数*马数
+};
+
+//操作时间
+Consts.TIME_CONF = {
+	//ai操作时间配置
+	ReqRobotTime: 300,														//玩家进入离开房间请求机器人的延时
+
+	//动画时间
+	GameStartAnimTime: 1000, 												//游戏开始动画时长
+	FaPaiAnimTime: 2000, 													//发牌时长
+
+	//流程时长
+	ReadyLeftTime: 12000, 													//准备时间
+	OutCardLeftTime: 10000, 												//打牌等待时长
+	OpeLeftTime: 5000, 														//吃碰杠胡等待时长
+};
+
+//机器人配置
+Consts.ROBOT = {
+	AutoReadyTime: { 														//自动准备时间
+		Min: 100,
+		Max: 200,
 	},
 
-	//操作类型
-	OPE_TYPE: {
-		NO_OPT: -1, 															//无操作
-		GUO: 0, 																//过
-		PENG: 1, 																//碰
-		GANG: 2,																//杠
-		AN_GANG: 3,																//暗杠
-		BU_GANG: 4,																//补杠
-		HU: 5,																	//胡
-		OUT_CARD: 6, 															//打牌
+	UpDataUserListLeave: { 													//房间中没有了真实玩家离开房间的时间
+		Min: 100,
+		Max: 300,
 	},
 
-	//倍数类型
-	RATE_TYPE: {
-		GANG: 1, 																//被人放杠
-		ZI_GANG: 2, 															//自摸杠
-		FANG_GANG: 3, 															//给别人放杠
-		BEI_ZI_GANG： 4, 														//被自摸杠
-		HU: 5, 																	//胡牌
-		BEI_HU: 6, 																//被胡牌
-		MO_MA: 7, 																//摸马
-		BEI_MA: 8, 																//被摸马
+	AutoOpeTime: { 															//自动操作的时间
+		Min: 1000,
+		Max: 2000,
 	},
 
-	//倍数配置
-	RATE_CONF: {
-		RATE_TYPE.GANG: 1, 														//倍数*(玩家数-1)
-		RATE_TYPE.ZI_GANG: 1, 													//倍数*(玩家数-1)
-		RATE_TYPE.FANG_GANG: -1, 												//倍数*(玩家数-1)
-		RATE_TYPE.BEI_ZI_GANG: -1, 												//倍数
-		RATE_TYPE.HU: 1, 														//倍数*(玩家数-1)
-		RATE_TYPE.BEI_HU: -1, 													//倍数
-		RATE_TYPE.MO_MA: 1, 													//倍数*(玩家数-1)*马数
-		RATE_TYPE.BEI_MA: -1, 													//倍数*马数
-	},
-
-	//操作时间
-	TIME_CONF: {
-		//ai操作时间配置
-		ReqRobotTime: 300,														//玩家进入离开房间请求机器人的延时
-
-		//动画时间
-		GameStartAnimTime: 1000, 												//游戏开始动画时长
-		FaPaiAnimTime: 2000, 													//发牌时长
-
-		//流程时长
-		ReadyLeftTime: 12000, 													//准备时间
-		OutCardLeftTime: 10000, 												//打牌等待时长
-		OpeLeftTime: 5000, 														//吃碰杠胡等待时长
-	},
-
-	//机器人配置
-	ROBOT: {
-		AutoReadyTime: { 														//自动准备时间
-			Min: 100,
-			Max: 200,
-		},
-
-		UpDataUserListLeave: { 													//房间中没有了真实玩家离开房间的时间
-			Min: 100,
-			Max: 300,
-		},
-
-		AutoOpeTime: { 															//自动操作的时间
-			Min: 1000,
-			Max: 2000,
-		},
-
-		AutoOutCardTime: { 														//自动出牌时间
-			Min: 1000,
-			Max: 5000,
-		},
+	AutoOutCardTime: { 														//自动出牌时间
+		Min: 1000,
+		Max: 5000,
 	},
 };
+
+module.exports = Consts;
