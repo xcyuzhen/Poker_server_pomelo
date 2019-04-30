612fe28625febe3abd01dfa864e0080c39dc044f
xcyuzhen <yuzhenxc@163.com>
2019-04-30 14:25:46 +0800

    更新服务端

--- game-server/app/models/mjFriendGroupConfig.js
+++ game-server/app/models/mjFriendGroupConfig.js
@@ -4,7 +4,7 @@ var friendGroupConfig = {
 		{typeIndex: 2, desc: "赢家支付"},
 	],
 	costType: 1, 										//扣除种类(1: 金币 2: 钻石)
-	costNumConfig: [ 										//扣除数量配置
+	costNumConfig: [ 									//扣除数量配置
 		{roundNum: 1, cost: 100},
 		{roundNum: 2, cost: 180},
 		{roundNum: 3, cost: 260},
