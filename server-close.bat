cb4c017937334c6f2fdd56ef7a12fa67f6ea338d
xcyuzhen <yuzhenxc@163.com>
2019-05-28 16:29:32 +0800

    提交批处理文件

---
+++ server-close.bat
@@ -0,0 +1,1 @@
+start cmd /k "taskkill /f /t /im node.exe &net stop Redis &taskkill /f /t /im cmd.exe"
---
+++ server-open.bat
@@ -0,0 +1,2 @@
+start cmd /k "net start Redis &cd web-server &&node app"
+start cmd /k "cd game-server &&pomelo start"
